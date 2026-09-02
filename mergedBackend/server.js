132635ff6c7439b3ffdeefb8977b63f7011faf49
kavish <kavisheksewa@gmail.com>
2026-09-02 13:40:42 +0530

    updang

--- mergedBackend/server.js
+++ mergedBackend/server.js
@@ -413,7 +413,7 @@ app.use(cors({
   origin: [
     "http://localhost:5173",
     "http://localhost:5174",
-    "https://kavi-shawls.vercel.app/"
+    "https://kavi-shawls.vercel.app"
   ],
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   credentials: true, // ✅ Zaroori hai taaki cookies frontend se backend jayein
