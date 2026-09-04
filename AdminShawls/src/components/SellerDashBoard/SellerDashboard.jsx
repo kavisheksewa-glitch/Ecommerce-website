a4cf96dd5006a36361ae3a7f9b7d3cebd982a8c6
kavish <kavisheksewa@gmail.com>
2026-09-04 11:00:39 +0530

    updating

--- AdminShawls/src/components/SellerDashBoard/SellerDashboard.jsx
+++ AdminShawls/src/components/SellerDashBoard/SellerDashboard.jsx
@@ -378,7 +378,7 @@ function SellerDashboard() {
               <div className="col-6 col-lg-4 col-md-6">
                 <div className="card shadow border-0 text-center p-4 Seller_card">
                   <h5>Delivered Orders</h5>
-                  <h2 className="text-success">{stats.deliveredOrders}</h2>
+                  <h1 className="text-success">{stats.deliveredOrders}</h1>
                 </div>
               </div>
 
