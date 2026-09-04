54441de65525d962f2df987918e332ac23b461f5
kavish <kavisheksewa@gmail.com>
2026-09-04 16:25:59 +0530

    upe

--- AdminShawls/src/component/Home/Home.jsx
+++ AdminShawls/src/component/Home/Home.jsx
@@ -948,6 +948,8 @@ function Home() {
             className="btn btn-outline-dark fw-semibold"
             onClick={() => navigate("/shop/featured")}
           >
+
+
             Explore Featured Shawls
           </button>
         </div>
