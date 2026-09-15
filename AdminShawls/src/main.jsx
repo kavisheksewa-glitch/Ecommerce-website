d72656f1d12a2138e71831d474a96628514a9ad6
kavish <kavisheksewa@gmail.com>
2026-09-15 12:23:21 +0530

    Add basename to BrowserRouter in main.jsx

--- AdminShawls/src/main.jsx
+++ AdminShawls/src/main.jsx
@@ -8,7 +8,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Ecommerce-website">
       <App />
     </BrowserRouter>
   </React.StrictMode>
