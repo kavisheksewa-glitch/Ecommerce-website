d2dccf7bfcdbe91e901ad68e847c1258888b8dfa
kavish <kavisheksewa@gmail.com>
2026-09-15 14:17:44 +0530

    api change1

--- mergedBackend/server.js
+++ mergedBackend/server.js
@@ -25,7 +25,7 @@ app.use(cors({
   origin: [
     "http://localhost:5173",
     "http://localhost:5174",
-    "https://kavi-shawls.vercel.app"
+    "https://kavi-shawls.vercel.app",
     "https://kavisheksewa-glitch.github.io"
   ],
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
