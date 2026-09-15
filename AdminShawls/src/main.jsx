d9cf87b72bde128a44d8674c976d677e2e45bb65
kavish <kavisheksewa@gmail.com>
2026-09-15 14:33:08 +0530

    api change1

--- AdminShawls/src/main.jsx
+++ AdminShawls/src/main.jsx
@@ -8,7 +8,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/Ecommerce-website">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
