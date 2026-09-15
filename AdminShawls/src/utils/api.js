69285e2899ca4794457db0446e5d4be6f2c727c5
kavish <kavisheksewa@gmail.com>
2026-09-15 09:58:18 +0530

    apidev

--- AdminShawls/src/utils/api.js
+++ AdminShawls/src/utils/api.js
@@ -1,7 +1,27 @@
-// src/utils/api.js
+// // src/utils/api.js
+// import axios from "axios";
+
+// const BASE_URL = "http://localhost:5000";
+
+// const API = axios.create({
+//   baseURL: BASE_URL,
+//   withCredentials: true,
+// });
+
+// export { BASE_URL };
+// export default API;
+
+
+
+
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000";
+// Local development → localhost
+// Vercel production → Render backend
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5000"
+    : "https://ecommerce-website-ggui.onrender.com";
 
 const API = axios.create({
   baseURL: BASE_URL,
