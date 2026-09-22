ac971c3472de6fbaf1daa59bebee505bc5872d2e
kavish <kavisheksewa@gmail.com>
2026-09-22 13:39:40 +0530

    all done1.11

--- AdminShawls/src/utils/api.js
+++ AdminShawls/src/utils/api.js
@@ -1,8 +1,8 @@
 // src/utils/api.js
 import axios from "axios";
 
-//const BASE_URL = "https://ecommerce-website-ggui.onrender.com";
-const BASE_URL = "http://localhost:5000";
+const BASE_URL = "https://ecommerce-website-ggui.onrender.com";
+//const BASE_URL = "http://localhost:5000";
 const API = axios.create({
   baseURL: BASE_URL,
   withCredentials: true,
